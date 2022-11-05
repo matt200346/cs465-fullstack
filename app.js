e61b4df9fcf3db1ee2e7830eea88a50cf64da3e4
matt200346 <matthew.eaton2@snhu.edu>
2022-11-04 23:18:27 -0400

    fixed landing page loading the wrong page by default

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/about', aboutRouter);
+app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
