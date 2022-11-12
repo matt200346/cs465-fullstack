00a20ce3025e7a93236d743ba7650d8c840d7c89
matt200346 <matthew.eaton2@snhu.edu>
2022-11-12 03:07:01 -0500

    Static HTML to JSON for Travel

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+
 const travel = (req, res) => {
     pageTitle = process.hasUncaughtExceptionCaptureCallback.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    res.render('travel', {title: pageTitle, trips});
 };
 
 module.exports = {
