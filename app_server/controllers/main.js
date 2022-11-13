9f5c221b2c1ba6a4d2406a641da449f6d41c5fe8
matt200346 <matthew.eaton2@snhu.edu>
2022-11-13 00:16:48 -0500

    Changed applicable pages from static HTML to JSON

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const indexNewsDetails = JSON.parse(fs.readFileSync('./data/news.json', 'utf8'));
+
 const index = (req, res) => {
-    res.render('index', {title: 'Travlr Getaways'});
+    pageTitle = process.hasUncaughtExceptionCaptureCallback.npm_package_description + ' - Main';
+    res.render('index', {title: pageTitle, indexNewsDetails});
 };
 
 module.exports = {
--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const mealDetails = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 const meals = (req, res) => {
     pageTitle = process.hasUncaughtExceptionCaptureCallback.npm_package_description + ' - Meals';
-    res.render('meals', {title: pageTitle});
+    res.render('meals', {title: pageTitle, mealDetails});
 };
 
 module.exports = {
