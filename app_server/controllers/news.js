db1c6e2036bc199273dbb987850feb534554d31a
matt200346 <matthew.eaton2@snhu.edu>
2022-11-12 22:51:53 -0500

    Changed static HTML to JSON for News and Rooms

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+const newsDetails = JSON.parse(fs.readFileSync('./data/news.json', 'utf8'));
+const tips = JSON.parse(fs.readFileSync('./data/tips.json', 'utf8'));
+const topNews = JSON.parse(fs.readFileSync('./data/topNews.json', 'utf8'));
+
 const news = (req, res) => {
     pageTitle = process.hasUncaughtExceptionCaptureCallback.npm_package_description + ' - news';
-    res.render('news', {title: pageTitle});
+    res.render('news', {title: pageTitle, newsDetails, tips, topNews});
 };
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const roomDetails = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 const rooms = (req, res) => {
     pageTitle = process.hasUncaughtExceptionCaptureCallback.npm_package_description + ' - rooms';
-    res.render('rooms', {title: pageTitle});
+    res.render('rooms', {title: pageTitle, roomDetails});
 };
 
 module.exports = {
