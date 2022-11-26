1d883f7e932b91ee35d35b4128006db6ceeee63d
matt200346 <matthew.eaton2@snhu.edu>
2022-11-26 08:59:10 -0500

    finished implementing RESTFUL api for travel

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,49 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
 
+// render travel list view
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    res.render('travel',
+        {
+            title: pageTitle,
+            trips: responseBody,
+            message
+        }
+    );
+}
 
-const travel = (req, res) => {
-    pageTitle = process.hasUncaughtExceptionCaptureCallback.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle, trips});
+// Get travel list view
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+    request(
+        requestOptions,
+        (err, { statuscode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    );
 };
 
 module.exports = {
-    travel
+    travelList
 };
